44da08c0f20912a21ce7c0ad3817a88f38e40acd
Bettina Meyer <bettina.meyer@nbi.ku.dk>
2018-11-15 14:04:39 +0100

    adding multiple nodes

--- script_submit_CP_jobs.sh
+++ script_submit_CP_jobs.sh
@@ -11,10 +11,23 @@
 # (try with multiple cores)
 
 dTh=2
+#th_params=( 2 3 )
 
-th_params=( 2 3 )
-z_params=( 500 1000 )
-r_params=( 2000 1000 )
+if [ $dTh -eq 3 ]
+then
+  z_params=( 500 1000 2000 )
+  r_params=( 2000 1000 500 )
+elif [ $dTh -eq 2 ]
+then
+  z_params=( 2450 1225 815 )
+  r_params=( 815 1225 2450 )
+elif [ $dTh -eq 1 ]
+then 
+  z_params=( 3465 1730 1155 )
+  r_params=( 1155 1730 3465 )
+fi
+
+
 
 n_geom=${#z_params[@]}
 n_therm=${#th_params[@]}
@@ -23,6 +36,8 @@ n_tot=$(( $n_geom*$n_therm ))
 echo "dTh:" $dTh
 echo "#geometry parameters:" $n_geom
 
+
+
 count_geom=0
 while [ $count_geom -lt $n_geom ]
 do
@@ -30,11 +45,24 @@ do
   zstar=${z_params[$count_geom]}
   rstar=${r_params[$count_geom]}
   echo "parameters:" $zstar $rstar
-  python generate_namelist_sbatch.py ColdPoolDry_single_3D --zstar $zstar --rstar $zstar --dTh $dTh
+  python generate_namelist_sbatch.py ColdPoolDry_single_3D --zstar $zstar --rstar $rstar --dTh $dTh
   
   echo "generated namelist file"
+  id="dTh"$dTh"_z"$zstar"_r"$rstar
+  echo $id
+  #python main_test.py "ColdPoolDry_single_3D_"$id".in"
+  python main_test.py "ColdPoolDry_single_3D.in"
+  
+  #sbatch SLURM_script_bm.sh ColdPoolDry_single_3D.in
+  #sbatch SLURM_script_bm.sh "ColdPoolDry_single_3D_"$id".in"
+  sbatch SLURM_script_bm.sh "ColdPoolDry_single_3D.in"
 
-  sbatch SLURM_script_bm.sh ColdPoolDry_single_3D.in
+  # use the sleep command to add delay fora  specified amoutn of time
+  # s for seconds (default); m for minutes; h for hours; d for days
+
+  cp ColdPoolDry_single_3D.in "ColdPoolDry_single_3D_"$id".in"
+  sleep 10
+
 
   ((count_geom++))
 done
