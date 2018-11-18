2bc87bdd6dd8a9b55d883920d32b09c2edd61cf9
Bettina Meyer <bettina.meyer@nbi.ku.dk>
2018-11-18 19:37:49 +0100

    debug Initialization

--- Initialization.pyx
+++ Initialization.pyx
@@ -554,8 +554,8 @@ def InitColdPoolDry_single_3D(namelist, Grid.Grid Gr,PrognosticVariables.Prognos
                 # k_max_arr[1, 2 * ic - i, 2 * jc - j] = k_max_arr[1, i, j]
                 # k_max_arr[1, i, 2 * jc - j] = k_max_arr[1, i, j]
                 if (r <= rstar):
-                    print('Smaller than rstar')
                     k_max = kstar * ( np.cos( r/rstar * np.pi / 2 ) ) ** 2
+                    print('Smaller than rstar: '+ str(k_max))
                     k_max_arr[0, i, j] = np.int(np.round(k_max))
                     k_max_arr[0, 2*ic_-i, j] = k_max_arr[0,i,j]
                     k_max_arr[0, 2*ic_-i, 2*jc_-j] = k_max_arr[0,i,j]
@@ -581,12 +581,12 @@ def InitColdPoolDry_single_3D(namelist, Grid.Grid Gr,PrognosticVariables.Prognos
         # qt_pert = (np.random.random_sample(Gr.dims.npg )-0.5)*0.025/1000.0
         double theta_pert_
 
-    Pa.root_print('Initialization')
-    Pa.root_print(theta.shape)
+    # Pa.root_print('Initialization')
+    # Pa.root_print(theta.shape)
 
     for i in xrange(Gr.dims.nlg[0]):
         for j in xrange(Gr.dims.nlg[1]):
-            Pa.root_print('ij' + str(i)+', '+str(j)+', '+str(Gr.dims.nlg[0])+', '+str(Gr.dims.nlg[1])+', '+str(k_max_arr[0,i,j])+', '+str(k_max_arr[1,i,j]))
+            # Pa.root_print('ij' + str(i)+', '+str(j)+', '+str(Gr.dims.nlg[0])+', '+str(Gr.dims.nlg[1])+', '+str(k_max_arr[0,i,j])+', '+str(k_max_arr[1,i,j]))
             if k_max_arr[1, i, j] > 0:
                 for k in xrange(Gr.dims.nlg[2]):
                     th = th_g
@@ -604,7 +604,7 @@ def InitColdPoolDry_single_3D(namelist, Grid.Grid Gr,PrognosticVariables.Prognos
         Py_ssize_t w_varshift = PV.get_varshift(Gr,'w')
         Py_ssize_t s_varshift = PV.get_varshift(Gr,'s')
 
-    Pa.root_print('Initialization')
+    # Pa.root_print('Initialization')
 
     for i in xrange(Gr.dims.nlg[0]):
         ishift = i * Gr.dims.nlg[1] * Gr.dims.nlg[2]
