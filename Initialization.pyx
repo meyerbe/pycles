6bf16a9eaa246d33577a0af5c5004fbba1e866b1
Bettina Meyer <bettina.meyer@nbi.ku.dk>
2018-11-19 11:30:33 +0100

    debug Initialization

--- Initialization.pyx
+++ Initialization.pyx
@@ -509,11 +509,14 @@ def InitColdPoolDry_single_3D(namelist, Grid.Grid Gr,PrognosticVariables.Prognos
                 PV.values[v_varshift + ijk] = 0.0
                 PV.values[w_varshift + ijk] = 0.0
 
-                r  = np.sqrt(((Gr.y_half[j + Gr.dims.indx_lo[1]]/1000.0 - 25.6)/4.0)**2.0
-                             + ((Gr.z_half[k + Gr.dims.indx_lo[2]]/1000.0 - 10.0)/1.2)**2.0)     # changed since VisualizationOutput defined in yz-plane
+                r  = np.sqrt(((Gr.y_half[j + Gr.dims.indx_lo[1]]/1000.0 - 1.5)/1.0)**2.0
+                             + ((Gr.z_half[k + Gr.dims.indx_lo[2]]/1000.0 - 1.0)/1)**2.0)     # changed since VisualizationOutput defined in yz-plane
                 r = fmin(r,1.0)   # cos(pi)=0
                 # r = np.sqrt( (Gr.x_half[i + Gr.dims.indx_lo[0]] - xc)**2 +
                 #          (Gr.y_half[j + Gr.dims.indx_lo[1]] - yc)**2 )
+                # k_max = (kstar + marg_i) * ( np.cos( r/(rstar + marg) * np.pi / 2 )) ** 2
+                # Pa.root_print(str(k_max) + ', ' + str(np.int(np.round(k_max))) )
+                # k_max_arr[1, i, j] = np.int(np.round(k_max))
                 th = (300.0 )*exner_c(RS.p0_half[k]) - 15.0*( cos(np.pi * r) + 1.0) /2.0
                 PV.values[s_varshift + ijk] = Th.entropy(RS.p0_half[k],th,0.0,0.0,0.0)
     Pa.root_print('Initialization: finished PV initialization')
