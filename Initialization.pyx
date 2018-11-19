791f33da7790482b47f781798762759ebe0666a2
Bettina Meyer <bettina.meyer@nbi.ku.dk>
2018-11-19 10:12:08 +0100

    debug Initialization

--- Initialization.pyx
+++ Initialization.pyx
@@ -549,14 +549,14 @@ def InitColdPoolDry_single_3D(namelist, Grid.Grid Gr,PrognosticVariables.Prognos
             # r = np.sqrt( (Gr.y_half[j + Gr.dims.indx_lo[1]] - yc)**2 )
             r = np.sqrt( (Gr.x_half[i + Gr.dims.indx_lo[0]] - xc)**2 +
                          (Gr.y_half[j + Gr.dims.indx_lo[1]] - yc)**2 )
-            Pa.root_print('r_1: '+str(r) +', '+str(rstar))
+            Pa.root_print('r_1: '+str(r) +', '+str(rstar) + ', ' + str(rstar+marg))
 
-    #         if (r <= (rstar + marg) ):
-    #             print('smaller than rstar + marg')
-    #             print('r/(rstar + marg): ' + str(r/(rstar / marg)))
-    #             print('r/rstar: ' + str(r/rstar))
-    #             k_max = (kstar + marg_i) * ( np.cos( r/(rstar + marg) * np.pi / 2 )) ** 2
-    #             k_max_arr[1, i, j] = np.int(np.round(k_max))
+            if (r <= (rstar + marg) ):
+                print('smaller than rstar + marg')
+                print('r/(rstar + marg): ' + str(r/(rstar / marg)))
+                print('r/rstar: ' + str(r/rstar))
+                k_max = (kstar + marg_i) * ( np.cos( r/(rstar + marg) * np.pi / 2 )) ** 2
+                k_max_arr[1, i, j] = np.int(np.round(k_max))
     #             k_max_arr[1, 2*ic_-i, j] = k_max_arr[1, i, j]
     #             k_max_arr[1, 2*ic_-i, 2*jc_-j] = k_max_arr[1, i, j]
     #             k_max_arr[1, i, 2*jc_-j] = k_max_arr[1, i, j]
